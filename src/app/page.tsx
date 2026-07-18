d955375970a6335e4af44366ad53266a81fd5210
VM-Kirubavathi <kirubavathi@tito.org.in>
2026-07-18 16:55:56 +0530

    Programs, Mental calisthenics, Form, Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import HeroSection from "@/app/components/HeroSection";
+import GetInTouch from "./components/contact";
+import CoursesOffered from "./components/CoursesOffered";
+import FormBooking from "./components/formbooking";
+import MentalCalisthenics from "./components/MentalCalisthenics";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <CoursesOffered />
+      <MentalCalisthenics />
+      <FormBooking />
+      <GetInTouch />
     </>
   );
 }
